8e29a396e549ea58a03f6d77dd1fcefb95ce5bb6
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-05-02 20:19:59 +0530

    final commit

--- src/modules/confirmation/confirmation.js
+++ src/modules/confirmation/confirmation.js
@@ -17,10 +17,11 @@ const Confirmation = () => {
 
   return (
     <>
-      <Typography variant="body2" color="textPrimary">
-        Thank you for the Booking. Click the below button to return to home page
-      </Typography>
-
+      <Typography
+        variant="body2"
+        color="textPrimary"
+      >Thank you for the Booking. Click the below button to return to home page</Typography>
+  
       <Button
         variant="outlined"
         color="primary"
